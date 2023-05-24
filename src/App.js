3e1ee3ecc8ddf7910211b92667994cc61ebc54a9
sayli01 <ranesayli1929@gmail.com>
2023-05-24 09:49:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,48 +9,21 @@ function App() {
 }
 
 function EventDemo() {
-  // Data Member
-  let [counter] = useState(100);
+  let [counter, setCounter] = useState(100);
 
-  // Member Funcn
   let likeMe = () => {
-    alert();
-  };
+    counter = counter + 1;
 
-  // default covention of the param name is :: e, p1
-  // e very userful when working input element
-  let likeMe1 = (e) => {
-    console.log(e);
-    console.log(e.target);
-  };
-
-  // User defined param :: e is not available
-  let likeMe2 = (p1) => {
-    console.log(p1);
-  };
-
-  // Both e and p1
-  let likeMe3 = (e, p1) => {
-    console.log(p1);
-    console.log(e);
+    // Trigger the DOM Operation :: Asking the REACT to perform DOM
+    setCounter(counter);
   };
 
   // UI
   return (
     <div>
+      <img src="https://picsum.photos/id/237/300" alt="" />
       <h1>Like {counter}</h1>
       <input type="button" value="Like Me" onClick={likeMe} />
-      <input type="button" value="Like Me 1" onClick={likeMe1} />
-
-      {/** For User Defined param */}
-      <input type="button" value="Like Me 2" onClick={() => likeMe2("data")} />
-
-      {/** Default Param + User Defined Param */}
-      <input
-        type="button"
-        value="Like Me 3"
-        onClick={(e) => likeMe3(e, "data")}
-      />
     </div>
   );
 }
