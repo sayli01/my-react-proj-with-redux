edea5ec588243f3084b2fbf292e83b5bfdb98fde
sayli01 <ranesayli1929@gmail.com>
2023-05-24 09:34:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -17,11 +17,18 @@ function EventDemo() {
     alert();
   };
 
+  // default covention of the param name is :: e, p1
+  let likeMe1 = (e) => {
+    console.log(e);
+    console.log(e.target);
+  };
+
   // UI
   return (
     <div>
       <h1>Like {counter}</h1>
       <input type="button" value="Like Me" onClick={likeMe} />
+      <input type="button" value="Like Me 1" onClick={likeMe1} />
     </div>
   );
 }
