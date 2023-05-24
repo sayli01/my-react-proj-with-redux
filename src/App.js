77a766c2895f29938e1920a74a8bb049076182c8
sayli01 <ranesayli1929@gmail.com>
2023-05-24 10:32:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -11,17 +11,31 @@ function App() {
 function ListDemo() {
   // let list = ["237"]; // stateless
   let [list, setList] = useState(["237"]); // statefull
+  let [imageInput, setImageInput] = useState("");
 
   let addNewImage = () => {
-    let newList = ["238", ...list];
+    // let newList = ["238", ...list];
+    let newList = [imageInput, ...list];
     setList(newList);
+  };
+
+  let inputImageHandler = (e) => {
+    setImageInput(e.target.value);
   };
 
   return (
     <div>
       <h1>List Demo</h1>
 
-      <input type="button" value="Add New Image" onClick={addNewImage} />
+      <div className="my-2">
+        <input
+          type="text"
+          placeholder="Enter Image Id"
+          value={imageInput}
+          onChange={inputImageHandler}
+        />
+        <input type="button" value="Add New Image" onClick={addNewImage} />
+      </div>
 
       {list.map((item, index) => (
         <EventDemo key={index} imageId={item} title="first" />
